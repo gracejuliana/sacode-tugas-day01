5bc9b65b0e110a4eae9aa630efcc05580de20bcf
gracejuliana <gracejuliana411@gmail.com>
2023-03-15 14:57:30 +0900

    variabel komplit

---
+++ variabel.php
@@ -0,0 +1,15 @@
+<?php
+    //contoh penulisan variabel didalam file PHP
+    $title ="SaCode";
+    $tag_line = "Papua Coding Scholl";
+    $tahun = "2023";
+
+    // Menampilkan data dari salam variabel
+    echo $title;
+    echo "<br>";
+    echo $tag_line;
+    echo "<br>";
+    echo $tahun;
+    
+ 
+?>
