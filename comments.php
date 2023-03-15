c020eb75c7a0f2790e1edf3c210d54f89e3bbfa5
gracejuliana <gracejuliana411@gmail.com>
2023-03-15 14:45:14 +0900

    tester comment.php

---
+++ comments.php
@@ -0,0 +1,12 @@
+<?php
+    // contoh komentar single-line di php
+
+    # ini juga contoh komentar single-line di php
+
+    /*
+    berikut ini contoh komentar untuk 
+    bisa diterapkan untuk menuliskan beberapa baris dalam komentar 
+
+    */
+    echo "PHP Comments";
+?>
